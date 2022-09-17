b5891023d830d38b0de7ec3dc80ea7f9c318f6fe
rob9315 <dev.robk@gmail.com>
2022-09-18 01:35:37 +0200

    cors options?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#![allow(clippy::let_unit_value)]
+
 mod bedrock;
 mod cors;
 mod java;
 
+use std::path::PathBuf;
 #[allow(unused)]
 use std::sync::atomic::AtomicUsize;
 
@@ -11,12 +14,15 @@ use trust_dns_resolver::TokioAsyncResolver;
 #[macro_use]
 extern crate rocket;
 
+#[options("/<_path..>")]
+pub fn options(_path: PathBuf) {}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(dns())
         .attach(Cors)
-        .mount("/", routes![java::java, bedrock::bedrock])
+        .mount("/", routes![java::java, bedrock::bedrock, options,])
 }
 
 fn dns() -> TokioAsyncResolver {
